78d30a4c061c49884f6190cc1083fc8c2ec02dac
Chris Reynolds <chris@humanmade.com>
2020-01-07 12:34:40 -0700

    load and bootstrap all the things

---
+++ plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: HM Basic Auth
+ * Description: Basic PHP authentication for HM Dev and Staging environments.
+ * Author: Human Made Limited
+ * Author URI: https://humanmade.com
+ * Version: 1.0
+ *
+ * @package HM\BasicAuth
+ */
+
+namespace HM\BasicAuth;
+
+require_once( 'inc/namespace.php' );
+require_once( 'inc/basic-auth.php' );
+
+// Kick it off.
+bootstrap();
+require_auth();
