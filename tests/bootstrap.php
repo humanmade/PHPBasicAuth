e5faaf4a91677b60db0ce98e33c92dce7689d06a
Chris Reynolds <chris@humanmade.com>
2020-04-09 11:53:13 -0600

    require the namespace not the plugin file we don't want to run the init hook and run basic auth

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,7 +27,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the files being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/plugin.php';
+	require dirname( dirname( __FILE__ ) ) . '/namespace.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
