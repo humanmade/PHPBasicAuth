a18dad98fa448b3b0bcabd003641dd0cb18d5c33
Robin Devitt <robin@humanmade.com>
2022-03-08 14:10:24 +0200

    HM Linter clean up.

--- inc/namespace.php
+++ inc/namespace.php
@@ -35,7 +35,7 @@ function is_development_environment() : bool {
 	do_action( 'hmauth_action_before_dev_env_check' );
 
 	$hm_dev        = defined( 'HM_DEV' ) && HM_DEV;
-	$altis_dev     = defined( 'HM_ENV_TYPE' ) && in_array( HM_ENV_TYPE, [ 'development', 'staging' ] );
+	$altis_dev     = defined( 'HM_ENV_TYPE' ) && in_array( HM_ENV_TYPE, [ 'development', 'staging' ], true );
 
 	/**
 	 * Allow our environments to be filtered outside of the plugin.
@@ -52,13 +52,13 @@ function is_development_environment() : bool {
 	}
 
 	// Don't require auth for AJAX requests.
-	$exclude_ajax       = ! defined( 'DOING_AJAX' ) || false === DOING_AJAX;
+	$exclude_ajax		= ! defined( 'DOING_AJAX' ) || false === DOING_AJAX;
 	// Don't require auth if we're in wp-cli.
-	$exclude_wp_cli     = ! defined( 'WP_CLI' ) || false === WP_CLI;
+	$exclude_wp_cli		= ! defined( 'WP_CLI' ) || false === WP_CLI;
 	// Don't require auth when running cron jobs.
-	$exclude_cron       = ! defined( 'DOING_CRON' ) || false === DOING_CRON;
+	$exclude_cron		= ! defined( 'DOING_CRON' ) || false === DOING_CRON;
 	// Don't require auth when installing or doing Altis health checks.
-	$exclude_installing = ! defined( 'WP_INSTALLING' ) || false === WP_INSTALLING;
+	$exclude_installing	= ! defined( 'WP_INSTALLING' ) || false === WP_INSTALLING;
 
 	$exclude = $exclude_ajax && $exclude_wp_cli && $exclude_cron && $exclude_installing;
 
